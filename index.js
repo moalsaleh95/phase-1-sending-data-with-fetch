a2d4d87ee3455d9b108b7318db2b7a9a004cf752
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-06-10 23:29:47 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+// // Add your code here
+function submitData(name, email) {
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        })
+    })
+        .then(res => res.json())
+        .then(obj => document.body.innerHTML = `${obj.name}, ${obj.email}, ${obj.id}`)
+        .catch(error => document.body.innerHTML = `"Message: ", ${error.message}`)
+}
